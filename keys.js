6a77a2293338e589737f2ad9c791a38f7085daa6
DianaLopez-Zen <1202.lopez.zendejas.diana@gmail.com>
2020-05-08 01:41:43 -0500

    Primer Comit

---
+++ keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'sql7.freemysqlhosting.net',
+        user: 'sql7338181',
+        password: 'T4fuw4Q92d',
+        database: 'sql7338181'
+    }
+    
+
+};
